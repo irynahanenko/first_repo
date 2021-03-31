fd84347d882c4e5488714930319024c74dbd114e
V I Hanenko <hanenko@vs-macbook-air.myfiosgateway.com>
2021-03-30 22:41:21 -0400

    try

---
+++ codewars/getDivisorsCnt.js
@@ -0,0 +1,11 @@
+// Count the number of divisors of a positive integer n.
+
+// Random tests go up to n = 500000.
+
+function getDivisorsCnt(n){
+    s = 0;
+    for(let i = 1; i <= n; i++){
+       if(n % i === 0) s++
+    }
+      return s;
+  } 
