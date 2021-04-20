4b7b5f0c2914a07598feadc03f7a1b17106b70aa
Iryna Hanenko <iryna.hanenko@gmail.com>
2021-04-19 23:18:51 -0400

    added files

---
+++ Automation/expected/urls.js
@@ -0,0 +1,21 @@
+export const URLs = {
+    elements: "https://demoqa.com/elements",
+    forms : "https://demoqa.com/forms",
+    alertsWindows : "https://demoqa.com/alertsWindows",
+    widgets : "https://demoqa.com/widgets",
+    interaction: "https://demoqa.com/interaction",
+    books: "https://demoqa.com/books",
+    textBox : 
+}
+​
+    export const labels = {
+        title: "ToolsQA",
+        footer: "© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.",
+        elements :"Elements",
+        forms: "Forms",
+        alerts: "Alerts, Frame & Windows",
+        widgets: "Widgets",
+        interactions: "Interactions",
+        books: "Book Store Application",
+        textBox : 
+    }
